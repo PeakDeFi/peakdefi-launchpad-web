c4ae048d64475f71184eba0c67e58bf0f0826a01
ivan_chaos <ivan.chaus@beleven.dev>
2022-01-03 06:43:19 +0200

    Finished totals panel styling

--- src/scenes/AllocationStaking/components/TotalsSection/components/TotalPanel/TotalPanel.js
+++ src/scenes/AllocationStaking/components/TotalsSection/components/TotalPanel/TotalPanel.js
@@ -2,15 +2,19 @@ import InfoIcon from '../../../StakingStats/images/InfoIcon.svg'
 
 import classes from './TotalPanel.module.scss'
 
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+}
+
 const TotalPanel = ({info}) => {
     return (<div className={classes.totalPanel}>
         <div className={classes.header}>
             <div className={classes.title}>{info.title}</div>
-            <img src={InfoIcon} />
+            <img src={InfoIcon} className={classes.infoIcon}/>
         </div>
         <div className={classes.body}>
-            <div className={classes.value}>{info.value.value} {info.value.append} <span className={classes.valueSubappend}>{info.value.subappend}</span></div>
-            <div className={classes.subvalue}> {info.subvalue.prepend}{info.subvalue.value}</div>
+            <div className={classes.value}>{numberWithCommas(info.value.value)} {info.value.append} <span className={classes.valueSubappend}>{info.value.subappend}</span></div>
+            <div className={classes.subvalue}> ~{info.subvalue.prepend}{numberWithCommas(info.subvalue.value)}</div>
         </div>
     </div>);
 }
