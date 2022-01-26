52e70dd23df8240b7f789163f1f6c22e45f1e074
ivan_chaos2 <ivan.chaus@beleven.dev>
2022-01-26 16:50:40 +0200

    hotfix#2

--- src/scenes/MainScreen/components/Table/components/TableHeader/TableHeader.js
+++ src/scenes/MainScreen/components/Table/components/TableHeader/TableHeader.js
@@ -4,16 +4,16 @@ import classes from "./TableHeader.module.scss"
 
 export function TableHeader(props) {
     return (
-        <div className={classes.TableHeader} style={{maxWidth: '100%', minWidth: "1000px"}}>
-            <div style={{width: '14%'}} >Project Name</div>
-            <div style={{width: '10%'}}>IDO Token Price</div>
-            <div style={{width: '9%'}}>Current Price</div>
-            <div style={{width: '9%'}}>ATH</div>
-            <div style={{width: '9%'}}>ATH IDO ROI</div>
-            <div style={{width: '12%'}}>No. Participants</div>
-            <div style={{width: '12%'}}>Total Raised</div>
-            <div style={{width: '12%'}}>Total Tokens Sold</div>
-            <div style={{width: '13%'}}>Sale Ended At</div>
+        <div className={classes.TableHeader} style={{maxWidth: '100%', minWidth: "900px"}}>
+            <div style={{width: '14%', paddingRight: '1em'}} >Project Name</div>
+            <div style={{width: '10%', paddingRight: '1em'}}>IDO Token Price</div>
+            <div style={{width: '9%', paddingRight: '1em'}}>Current Price</div>
+            <div style={{width: '8%', paddingRight: '1em'}}>ATH</div>
+            <div style={{width: '10%', paddingRight: '1em'}}>ATH IDO ROI</div>
+            <div style={{width: '12%', paddingRight: '1em'}}>No. Participants</div>
+            <div style={{width: '12%', paddingRight: '1em'}}>Total Raised</div>
+            <div style={{width: '12%', paddingRight: '1em'}}>Total Tokens Sold</div>
+            <div style={{width: '13%', paddingRight: '1em'}}>Sale Ended At</div>
         </div>
     )
 }
--- src/scenes/MainScreen/components/Table/components/TableRow/TableRow.js
+++ src/scenes/MainScreen/components/Table/components/TableRow/TableRow.js
@@ -12,7 +12,7 @@ export function TableRow(props) {
 
     const dispatch = useDispatch();
     return (
-        <div className={classes.TableRow} style={{maxWidth: '100%', minWidth: "1000px", background:props.color}} onClick={()=>dispatch(setSelectedIDO({...props}))}>
+        <div className={classes.TableRow} style={{maxWidth: '100%', minWidth: "900px", background:props.color}} onClick={()=>dispatch(setSelectedIDO({...props}))}>
             <div className={classes.infoBlock} style={{ width: '14%'}} >
                 <img alt={props.name} src={props.img} />
                 <div className={classes.info}>
@@ -26,8 +26,8 @@ export function TableRow(props) {
             </div>
             <div className={classes.divUpdate} style={{width: '10%'}}> {'$'+props.idoPrice} </div>
             <div className={classes.divUpdate} style={{width: '9%'}}> {'$' + props.currentPrice} </div>
-            <div className={classes.divUpdate} style={{width: '9%'}}>{'$' + props.ath }</div>
-            <div className={classes.divUpdate} style={{width: '9%'}}>{ props.roi.toFixed(3) + 'x' }</div>
+            <div className={classes.divUpdate} style={{width: '8%'}}>{'$' + props.ath }</div>
+            <div className={classes.divUpdate} style={{width: '10%'}}>{ props.roi.toFixed(3) + 'x' }</div>
             <div className={classes.divUpdate} style={{width: '12%'}}>{ props.partisipants }</div>
             <div className={classes.divUpdate} style={{width: '12%'}}>{ '$' + numberWithCommas(props.totalRaised)} </div>
             <div className={classes.divUpdate} style={{width: '12%'}}> {props.totalTokenSold} </div>
