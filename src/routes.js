f3dbe4f9ebf49ee5a40ea35e8a6dd56473f4570e
ivan_chaos <ivan.chaus@beleven.dev>
2022-03-11 21:21:28 +0200

    password protected page, hardcoded

--- src/routes.js
+++ src/routes.js
@@ -3,36 +3,48 @@ import IdoDetail from "./scenes/IdoDetail/IdoDetail"
 import AllocationStaking from "./scenes/AllocationStaking/AllocationStaking"
 import AdminPanel from "./scenes/AdminPanel/AdminPanel"
 import SalesPage from "./scenes/SalesPage/SalesPage"
+import Login from "./scenes/Login/Login"
 
 export const routes = [
   {
     path: "/",
     exact: true,
-    component: <MainScreen />
-    
+    component: <MainScreen />,
+    isPrivate: true
   },
   
   {
     path: "/project-details",
     exact: true,
-    component: <IdoDetail />
-    
+    component: <IdoDetail />,
+    isPrivate: true
   },
   
   {
     path: "/allocation-staking",
     exact: true,
-    component: <AllocationStaking />
+    component: <AllocationStaking />,
+    isPrivate: true
   },
 
   {
     path: '/sales',
     exact: true,
-    component: <SalesPage />
+    component: <SalesPage />,
+    isPrivate: true
   },
+
   {
     path: "/admin-panel",
     exact: true,
-    component: <AdminPanel />
+    component: <AdminPanel />,
+    isPrivate: true
+  },
+
+  {
+    path: '/login',
+    exact: true,
+    component: <Login />,
+    isPrivate: false
   }
 ]
