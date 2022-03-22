118250e4dd0358e7291288207d2ffa9763b0dac8
ivan_chaos <ivan.chaus@beleven.dev>
2022-03-22 11:56:44 +0200

    Popup completed, need a new logo

--- src/scenes/MainScreen/MainScreen.js
+++ src/scenes/MainScreen/MainScreen.js
@@ -3,10 +3,13 @@ import classes from './MainScreen.module.scss'
 import InfoBlock from './components/InfoBlock/Info'
 import IDO from './components/IDOBlock/IDO'
 import { useNavigate } from "react-router-dom";
+import GiveAwayPanel from "./components/GiveawayPanel/GiveawayPanel";
 
 
 const MainScreen = () => {
     const [mainText, setMainText] = useState(<>Enter the gateway<br></br> of Blockchain Projects</>);
+
+    const [showGiveaway, setShowGiveaway] = useState(true);
     const navigate = useNavigate();
     
     return (<div className={classes.MainSCreen}>
@@ -19,6 +22,7 @@ const MainScreen = () => {
 
         <InfoBlock />
         <IDO />
+        <GiveAwayPanel show={showGiveaway} setShow={setShowGiveaway}/>
     </div>);
 }
 
