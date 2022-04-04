7ef0c5c7241ed14245539043cb48ffb7ed86b9c1
ivan_chaos <ivan.chaus@beleven.dev>
2022-04-04 22:22:06 +0300

    Description text and start dates/text switch

--- src/scenes/MainScreen/components/IDOBlock/components/UpcomingIdoBlock/UpcomingIdoBlock.js
+++ src/scenes/MainScreen/components/IDOBlock/components/UpcomingIdoBlock/UpcomingIdoBlock.js
@@ -37,6 +37,8 @@ export function UpcomingIdoBlock({ props }) {
     const start_date = props.saleInfo.start_date ? ("0" + props.saleInfo.start_date.getDate()).slice(-2) + "." + ("0" + (props.saleInfo.start_date.getMonth() + 1)).slice(-2) + "." +
         props.saleInfo.start_date.getFullYear() : '';
 
+    
+    var options = { year: 'numeric', month: 'long', day: 'numeric' };
     return (
         <div className={classes.IdoBlock}>
             <header>
@@ -88,7 +90,9 @@ export function UpcomingIdoBlock({ props }) {
         
                         <div className={classes.textToShowBlock} >
 
-                            {textToShow("Start Date", props.timeline.sale_timeline_text)}
+                            {textToShow(
+                                "Start Date", props.timeline.show_text ? props.timeline.sale_timeline_text : new Date(props.timeline.registration_start*1000).toLocaleDateString('en-US', options)
+                            )}
                         </div>
                         <div className={classes.launchDetaid}>
                             <div className={classes.block}>
@@ -109,7 +113,7 @@ export function UpcomingIdoBlock({ props }) {
                     {activeSection===1 && <>
                         <div className={classes.descriptionSection}>
                             <h1>{props.token.name}</h1>
-                            <p>{props.heading_text}</p>
+                            <p>{props.short_description}</p>
                         </div>
                     </>}
                     
