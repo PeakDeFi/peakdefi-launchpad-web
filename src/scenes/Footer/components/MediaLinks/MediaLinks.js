8a3eabb172b0e10f7c79c5a0d827d240a496b529
ivan_chaos <ivan.chaus@beleven.dev>
2022-04-09 15:57:24 +0300

    added email address

--- src/scenes/Footer/components/MediaLinks/MediaLinks.js
+++ src/scenes/Footer/components/MediaLinks/MediaLinks.js
@@ -1,36 +1,37 @@
 import { useState } from 'react';
 import classes from './MediaLinks.module.scss'
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
 
 const MediaLinks = () => {
-    
+
     const [mediaLinks, setMediaLinks] = useState([
-        { 
-            icon: "https://peakdefi.com/assets/img/icons/telegram.svg", 
+        {
+            icon: "https://peakdefi.com/assets/img/icons/telegram.svg",
             link: "https://t.me/peakdefi_official"
         },
 
-        { 
-            icon: "https://peakdefi.com/assets/img/icons/discord.svg", 
+        {
+            icon: "https://peakdefi.com/assets/img/icons/discord.svg",
             link: "https://discord.gg/3ENv5QCtcs"
         },
 
-        { 
-            icon: "https://peakdefi.com/assets/img/icons/twitter.svg", 
+        {
+            icon: "https://peakdefi.com/assets/img/icons/twitter.svg",
             link: "https://twitter.com/peakdefi"
         },
 
-        { 
-            icon: "https://peakdefi.com/assets/img/icons/medium.svg", 
+        {
+            icon: "https://peakdefi.com/assets/img/icons/medium.svg",
             link: "https://medium.com/@PeakDeFi"
         },
 
-        { 
-            icon: "https://peakdefi.com/assets/img/icons/github.svg", 
+        {
+            icon: "https://peakdefi.com/assets/img/icons/github.svg",
             link: "https://github.com/peakdefi"
         },
 
-        { 
-            icon: "https://peakdefi.com/assets/img/icons/defipulse.svg", 
+        {
+            icon: "https://peakdefi.com/assets/img/icons/defipulse.svg",
             link: "https://defipulse.com/"
         }
     ])
@@ -38,11 +39,18 @@ const MediaLinks = () => {
     return (<div className={classes.MediaLinks}>
         <h6>© 2022 PEAK Labs</h6>
         <div className={classes.media}>
-            {mediaLinks.map(media=>
+            {mediaLinks.map(media =>
                 <a href={media.link}><img src={media.icon} /></a>
             )}
+
+            <div className={classes.verticalSeparator}></div>
+            <div className={classes.emailSection}>
+                <MailOutlineIcon style={{ fontSize: '1em', color: 'white', marginRight: '0.4em' }} />
+                <p className={classes.emailAddress}>launchpad@peakdefi.com</p>
+            </div>
+
         </div>
     </div>);
 }
- 
+
 export default MediaLinks;
