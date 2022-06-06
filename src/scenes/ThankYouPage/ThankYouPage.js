d34c3ce796e9650f5bf32752e1b64603e6a84335
ivan_chaos <ivan.chaus@beleven.dev>
2022-06-06 11:57:45 +0300

    initial components

---
+++ src/scenes/ThankYouPage/ThankYouPage.js
@@ -0,0 +1,9 @@
+import classes from './ThankYouPage.module.scss'
+
+const ThankYouPage = () => {
+    return (<div className={classes.ThankYouPage}>
+
+    </div>);
+}
+ 
+export default ThankYouPage;
