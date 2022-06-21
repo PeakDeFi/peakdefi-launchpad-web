c78f3fd4b692ea2e94bbb950d2933603bb41e2d0
ivan_chaos <ivan.chaus@beleven.dev>
2022-06-21 13:05:51 +0300

    api integration

--- src/scenes/AllocationStaking/API/staking.js
+++ src/scenes/AllocationStaking/API/staking.js
@@ -13,3 +13,7 @@ export const addReferrer = (refereeAddress, referrerAddress) => {
         }
     )
 }
+
+export const getLeaderboardData = () =>{
+    return axios.get(process.env.REACT_APP_API_URL+'leaderboard');
+}
--- src/scenes/AllocationStaking/components/Leaderboard/Leaderboard.js
+++ src/scenes/AllocationStaking/components/Leaderboard/Leaderboard.js
@@ -1,62 +1,44 @@
 import classes from './Leaderboard.module.scss'
 import StarIcon from './images/StarIcon.svg'
 import Table from '../Table/Table';
+import { useEffect, useState } from 'react';
+import { getLeaderboardData } from '../../API/staking';
 
 const Leaderboard = () => {
 
     const columns = [
-        {name: 'no', title: '#'},
-        {name: 'wallet', title: 'Nickname'},
-        {name: 'number_of_referrals',  title: 'Number of Referrals'},
-        {name: 'number_of_rewards', title: 'Number of Rewards'},
+        { name: 'no', title: '#' },
+        { name: 'wallet', title: 'Nickname' },
+        { name: 'number_of_referrals', title: 'Number of Referrals' },
+        { name: 'number_of_rewards', title: 'Number of Rewards' },
     ]
 
-    const data = [
-        {no: 1, wallet: '0x000045345', number_of_referrals: 12, number_of_rewards: 2000},
-        {no: 2, wallet: '0x000045345', number_of_referrals: 12, number_of_rewards: 2000},
-        {no: 3, wallet: '0x000045345', number_of_referrals: 12, number_of_rewards: 2000},
-        {no: 4, wallet: '0x000045345', number_of_referrals: 12, number_of_rewards: 2000},
-        {no: 5, wallet: '0x000045345', number_of_referrals: 12, number_of_rewards: 2000},
-        {no: 6, wallet: '0x000045345', number_of_referrals: 12, number_of_rewards: 2000},
-        {no: 7, wallet: '0x000045345', number_of_referrals: 12, number_of_rewards: 2000},
-        {no: 8, wallet: '0x000045345', number_of_referrals: 12, number_of_rewards: 2000},
-        {no: 9, wallet: '0x000045345', number_of_referrals: 12, number_of_rewards: 2000},
-        {no: 10, wallet: '0x000045345', number_of_referrals: 12, number_of_rewards: 2000},
-        {no: 11, wallet: '0x000045345', number_of_referrals: 12, number_of_rewards: 2000},
-        {no: 12, wallet: '0x000045345', number_of_referrals: 12, number_of_rewards: 2000},
-        {no: 13, wallet: '0x000045345', number_of_referrals: 12, number_of_rewards: 2000},
-        {no: 14, wallet: '0x000045345', number_of_referrals: 12, number_of_rewards: 2000},
-        {no: 15, wallet: '0x000045345', number_of_referrals: 12, number_of_rewards: 2000},
-        {no: 16, wallet: '0x000045345', number_of_referrals: 12, number_of_rewards: 2000},
-        {no: 17, wallet: '0x000045345', number_of_referrals: 12, number_of_rewards: 2000},
-        {no: 18, wallet: '0x000045345', number_of_referrals: 12, number_of_rewards: 2000},
-        {no: 19, wallet: '0x000045345', number_of_referrals: 12, number_of_rewards: 2000},
-        {no: 20, wallet: '0x000045345', number_of_referrals: 12, number_of_rewards: 2000},
-        {no: 21, wallet: '0x000045345', number_of_referrals: 12, number_of_rewards: 2000},
-        {no: 22, wallet: '0x000045345', number_of_referrals: 12, number_of_rewards: 2000},
-        {no: 23, wallet: '0x000045345', number_of_referrals: 12, number_of_rewards: 2000},
-        {no: 24, wallet: '0x000045345', number_of_referrals: 12, number_of_rewards: 2000},
-        {no: 25, wallet: '0x000045345', number_of_referrals: 12, number_of_rewards: 2000},
-        {no: 26, wallet: '0x000045345', number_of_referrals: 12, number_of_rewards: 2000},
-        {no: 27, wallet: '0x000045345', number_of_referrals: 12, number_of_rewards: 2000},
-        {no: 1, wallet: '0x000045345', number_of_referrals: 12, number_of_rewards: 2000},
-        {no: 1, wallet: '0x000045345', number_of_referrals: 12, number_of_rewards: 2000},
-        {no: 1, wallet: '0x000045345', number_of_referrals: 12, number_of_rewards: 2000},
-        {no: 1, wallet: '0x000045345', number_of_referrals: 12, number_of_rewards: 2000},
-        {no: 1, wallet: '0x000045345', number_of_referrals: 12, number_of_rewards: 2000},
-        {no: 1, wallet: '0x000045345', number_of_referrals: 12, number_of_rewards: 2000}
-    ]
+    const [leaderBoardData, setLeaderboardData] = useState([]);
+
+    useEffect(() => {
+        getLeaderboardData().then((response) => {
+            setLeaderboardData(response.data.leaders.map(e => {
+                return { no: e[0], wallet: e[1], number_of_referrals: e[2], number_of_rewards: e[3] }
+            }))
+        })
+    }, [])
+
 
     return (<div className={classes.Leaderboard}>
-        <header>
-            <img src={StarIcon} />
-            <h1>Leaderboard</h1>
-        </header>
-        <main>
-            <div className={classes.table}>
-                <Table columns={columns} data={data}/>
-            </div>
-        </main>
+        {leaderBoardData.length > 0 && <>
+            <header>
+                <img src={StarIcon} />
+                <h1>Leaderboard</h1>
+            </header>
+            <main>
+                <div className={classes.table}>
+                    <Table columns={columns} data={leaderBoardData} />
+                </div>
+            </main>
+
+        </>}
+
+
     </div>);
 }
 
