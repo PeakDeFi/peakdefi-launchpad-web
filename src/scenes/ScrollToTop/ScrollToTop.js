cfa9e01a1b60f75f266329e4358f7f2e83be663c
gladiypavlo@gmail.com <qwedsazxc@2000>
2022-09-18 20:04:54 +0300

    add 1 more place to save referrer

--- src/scenes/ScrollToTop/ScrollToTop.js
+++ src/scenes/ScrollToTop/ScrollToTop.js
@@ -1,8 +1,30 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import { useCookies } from 'react-cookie';
+import { useSearchParams } from "react-router-dom";
 
 export default function ScrollToTop() {
   const { pathname } = useLocation();
+  const [cookies, setCookie] = useCookies(['referrer_wallet_address']);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const saveReferrerWallet = () => {
+
+      if (!cookies.referrer_wallet_address && searchParams.get("referrer_wallet_address")) {
+          setCookie(
+              'referrer_wallet_address',
+              searchParams.get("referrer_wallet_address"),
+              {
+                  expires: new Date(new Date().setMonth(new Date().getMonth() + 1))
+              }
+          )
+
+      }
+  }
+  
+  useEffect(() => {
+          saveReferrerWallet();
+  }, []);
 
   useEffect(() => {
     window.scrollTo(0, 0);
