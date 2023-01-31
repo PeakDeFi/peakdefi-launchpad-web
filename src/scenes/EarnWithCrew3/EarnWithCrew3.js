9b2da6a879f97677d7f1c93007e8f9db41dc54ae
ivan131013 <ivan.chaus@beleven.dev>
2023-01-31 14:51:28 +0200

    added crew3 page

---
+++ src/scenes/EarnWithCrew3/EarnWithCrew3.js
@@ -0,0 +1,36 @@
+import classes from "./EarnWithCrew3.module.scss";
+
+const EarnWithCrew3 = () => {
+  return (
+    <div className={classes.EarnWithCrew3}>
+      <heading className={classes.heading}>
+        Earn up to 2100 BUSD monthly!
+      </heading>
+      <heading className={classes.subheading}>
+        On crew3 you can finalize simple tasks for PEAKDEFI and earn money every
+        month!
+      </heading>
+
+      <iframe
+        className={classes.video}
+        src="https://www.youtube.com/embed/Fk7T9Gibou8"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
+      <button
+        className={classes.ctaButton}
+        onClick={() => {
+          window.open(
+            "https://crew3.xyz/c/peakdeficommunity/invite/m20mqr_pjBUFS8y_En2nY"
+          );
+        }}
+      >
+        Go to CREW3 now!
+      </button>
+    </div>
+  );
+};
+
+export default EarnWithCrew3;
