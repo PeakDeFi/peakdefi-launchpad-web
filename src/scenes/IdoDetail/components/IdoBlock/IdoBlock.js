a1d62f7af25b8de9dfb71b2e66c3a469304ebc09
gladiypavlo@gmail.com <qwedsazxc@2000>
2023-04-25 10:09:48 +0300

    add prod build

--- src/scenes/IdoDetail/components/IdoBlock/IdoBlock.js
+++ src/scenes/IdoDetail/components/IdoBlock/IdoBlock.js
@@ -630,11 +630,11 @@ const IdoBlock = ({ idoInfo, ido, media }) => {
       </div>
 
       <div className={classes.actions}>
-        {isLotteryWinner && depositedAmount === 0 && (
+        {/* {isLotteryWinner && depositedAmount === 0 && (
           <div className={classes.lotteryWinner}>
             <h2>Lottery Winner!</h2>
           </div>
-        )}
+        )} */}
         <div className={classes.actionBlock}>
           {isAllowedToParticipate && (
             // && depositedAmount === 0
