32b9f177690f79c1a1aa5a6d788bed58567e49e3
gladiypavlo@gmail.com <qwedsazxc@2000>
2023-08-20 10:44:41 +0300

    change deposit recomendation

--- src/scenes/IdoDetail/components/IdoBlock/IdoBlock.js
+++ src/scenes/IdoDetail/components/IdoBlock/IdoBlock.js
@@ -540,7 +540,7 @@ const IdoBlock = ({ idoInfo, ido, media }) => {
 
   if (ido === undefined) return <></>;
 
-  const tierAllocation = [40, 150, 370, 500, 950, 1800];
+  const tierAllocation = [70, 150, 260, 390, 750, 1150];
 
   return (
     <div className={classes.IdoBlock}>
