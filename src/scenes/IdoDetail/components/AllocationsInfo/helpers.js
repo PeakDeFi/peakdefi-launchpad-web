e1af7efc60e949c5f4a5f369eee76d15118768ea
Morwo128 <volodimirg7@gmail.com>
2024-05-12 08:30:16 +0300

    Added prod build

--- src/scenes/IdoDetail/components/AllocationsInfo/helpers.js
+++ src/scenes/IdoDetail/components/AllocationsInfo/helpers.js
@@ -7,7 +7,7 @@ export const contractAddressFetcher = (sale_name) => {
     return "0xB3D11c5B7E955302d785a20B8B69538F257624CE";
   }
   if (sale_name?.toLowerCase() === "vendetta") {
-    return "0x0cb4a12b298244a56b7bdFC26Eb3A1D1e5dcFaBa";
+    return "0x49c1a1037D5b22E18f8c4F4aE8377f6833f92f87";
   }
   if (sale_name?.toLowerCase() === "octavia") {
     return "0xADc0ACe4dE87923Ea2DeFF82e2e69112362e9C32";
@@ -19,7 +19,7 @@ export const tgeContractAddressFetcher = (sale_name) => {
     return "0x56473A8F9388b8185004a86044649eDc4e70f16F";
   }
   if (sale_name?.toLowerCase() === "vendetta") {
-    return "0x0cb4a12b298244a56b7bdFC26Eb3A1D1e5dcFaBa";
+    return "0x49c1a1037D5b22E18f8c4F4aE8377f6833f92f87";
   }
   if (sale_name?.toLowerCase() === "anote") {
     return "0xB3D11c5B7E955302d785a20B8B69538F257624CE";
