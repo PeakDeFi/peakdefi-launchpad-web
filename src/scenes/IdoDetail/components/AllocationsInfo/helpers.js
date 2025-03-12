e44e364e0fde01d46078a14caf7086e81af0bf73
VolodymyrHladii <volodymyr.hladii@beleven.dev>
2025-03-12 08:23:58 +0200

    Fix vendeta

--- src/scenes/IdoDetail/components/AllocationsInfo/helpers.js
+++ src/scenes/IdoDetail/components/AllocationsInfo/helpers.js
@@ -6,7 +6,7 @@ export const contractAddressFetcher = (sale_name) => {
     return "0xd38ACf6CD7d840B64dEA325bf4bE0A27fba2b696";
   }
   if (sale_name?.toLowerCase() === "vendetta") {
-    return "0x49c1a1037D5b22E18f8c4F4aE8377f6833f92f87";
+    return "0x21E0034eA7212abDb0Ae3670ab4EbDc5d5Bf5D59";
   }
   if (sale_name?.toLowerCase() === "octavia") {
     return "0xE0D35BEb22A03207a365c0f82dDdA1a7AA1Ae030";
@@ -27,7 +27,7 @@ export const tgeContractAddressFetcher = (sale_name) => {
     return "0x899e0D7bd342ECE88632c8F35A256568DFb7F6cc";
   }
   if (sale_name?.toLowerCase() === "vendetta") {
-    return "0x49c1a1037D5b22E18f8c4F4aE8377f6833f92f87";
+    return "0x21E0034eA7212abDb0Ae3670ab4EbDc5d5Bf5D59";
   }
   if (sale_name?.toLowerCase() === "anote") {
     return "0xd38ACf6CD7d840B64dEA325bf4bE0A27fba2b696";
